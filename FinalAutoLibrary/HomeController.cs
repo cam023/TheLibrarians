109ac2e34c314039846285242fa2f3080d9526fe
cam023 <chrismolto@comcast.net>
2017-04-16 21:34:52 -0400

    Add files via upload

---
+++ FinalAutoLibrary/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoLibraryFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult AdminView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminView(FormCollection collection)
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Users ="admin")]
+        public ActionResult Payments()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
